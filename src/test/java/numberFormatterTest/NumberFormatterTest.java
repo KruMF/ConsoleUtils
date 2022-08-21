89939e75bcb40143af7739e4cda73ed0f38675ef
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-08-22 00:44:39 +0300

    0.2
    * console buffer with abstraction
    * separate separator string
    * test improvements
    * javadocs
    * readme update
    * inspection

--- src/test/java/numberFormatterTest/NumberFormatterTest.java
+++ src/test/java/numberFormatterTest/NumberFormatterTest.java
@@ -1,12 +1,29 @@
 package numberFormatterTest;
 
-import static consoleUtils.ConsoleUtils.*;
+import consoleUtils.ConsoleBuffer;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import static consoleUtils.ConsoleUtils.separatorString;
 import static consoleUtils.NumberFormatter.doubleToString;
 
 /**
- * Testing text formatting.
+ * Testing text formatter with console buffer.
  */
 public class NumberFormatterTest {
+    private static final String TEST_NAME = "Number formatter test";
+
+    private static final ConsoleBuffer consoleBuffer = new ConsoleBuffer() {
+        @Override
+        public List<String> header() {
+            return new ArrayList<>() {{
+                add(TEST_NAME + ":");
+                add(null);
+            }};
+        }
+    };
+
     private static final double
             d1 = 7.007,
             d2 = 12341234.1234,
@@ -18,15 +35,21 @@ public class NumberFormatterTest {
      *
      * @param args Command-line arguments.
      */
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-        printLine(doubleToString(d1,2));
-        printLine(doubleToString(d1,3));
-        printLine(doubleToString(d1,4));
-        separator();
-        printLine(doubleToString(d2,0));
-        printLine(doubleToString(d2,4));
-        separator();
-        printLine(doubleToString(d3,0));
-        printLine(doubleToString(d3,2));
+        addLine(doubleToString(d1,2));
+        addLine(doubleToString(d1,3));
+        addLine(doubleToString(d1,4));
+        addLine(separatorString());
+        addLine(doubleToString(d2,0));
+        addLine(doubleToString(d2,4));
+        addLine(separatorString());
+        addLine(doubleToString(d3,0));
+        addLine(doubleToString(d3,2));
+        consoleBuffer.print();
+    }
+
+    private static void addLine(String line) {
+        consoleBuffer.addLine(line);
     }
 }
