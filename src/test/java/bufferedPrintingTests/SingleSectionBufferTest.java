a7a53b2ef3030d2aa2a483260c8ac5aafdf0ed45
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-02-04 22:11:33 +0200

    work on tests

--- src/test/java/bufferedPrintingTests/SingleSectionBufferTest.java
+++ src/test/java/bufferedPrintingTests/SingleSectionBufferTest.java
@@ -1,6 +1,17 @@
 package bufferedPrintingTests;
 
-//
+/**
+ * Testing single-section console buffer.
+ * TODO: finish this
+ */
 public class SingleSectionBufferTest {
-    //
+    /**
+     * Main method of this test.
+     * Run this to start.
+     *
+     * @param args Command-line arguments.
+     */
+    public static void main(String[] args) {
+        //
+    }
 }
