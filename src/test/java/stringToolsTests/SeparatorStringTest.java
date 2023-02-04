0b4595b34478afcff7428c1ebc5c79468e88c7a4
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2023-02-04 04:41:00 +0200

    work on tests

--- src/test/java/stringToolsTests/SeparatorStringTest.java
+++ src/test/java/stringToolsTests/SeparatorStringTest.java
@@ -1,6 +1,55 @@
 package stringToolsTests;
 
-//TODO: finish this
+import java.util.List;
+import java.util.ArrayList;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static consoleUtils.SimplePrinting.printLineList;
+import consoleUtils.stringTools.SeparatorString;
+
+/**
+ * Testing separator strings.
+ */
 public class SeparatorStringTest {
-    //
+    private static final @NotNull String
+            REPEATABLE_PATTERN = "abc ",
+            CUSTOM_SEPARATOR = "custom";
+    private static final int PATTERN_LENGTH = 3;
+
+    /**
+     * Main method of this test.
+     * Run this to start.
+     *
+     * @param args Command-line arguments.
+     */
+    public static void main(String[] args) {
+        @NotNull List<@Nullable String> lines = new ArrayList<>() {{
+            add(getDefaultSeparator().getString());
+            add(null);
+            add(getRepeatingNullSeparator().getString());
+            add(null);
+            add(getRepeatingPatternSeparator().getString());
+            add(null);
+            add(getCustomSeparator().getString());
+        }};
+        printLineList(lines);
+    }
+
+    private static @NotNull SeparatorString getDefaultSeparator() {
+        return new SeparatorString();
+    }
+
+    private static @NotNull SeparatorString getRepeatingNullSeparator() {
+        return new SeparatorString(null, PATTERN_LENGTH);
+    }
+
+    private static @NotNull SeparatorString getRepeatingPatternSeparator() {
+        return new SeparatorString(REPEATABLE_PATTERN, PATTERN_LENGTH);
+    }
+
+    private static @NotNull SeparatorString getCustomSeparator() {
+        return new SeparatorString(CUSTOM_SEPARATOR);
+    }
 }
